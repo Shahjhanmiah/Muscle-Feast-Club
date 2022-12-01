b82b96c4d442b99f90aa426e7ee6824af24724a4
mdshahjhan <mdshahjha478877@gmail.com>
2022-12-01 11:33:42 +0600

    resutn

--- src/Activity/Activity.js
+++ src/Activity/Activity.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import './Activity.css'
 
 const Activity = (props) => {
     const { activity, clickHandler } = props
     const { id, img, name, trainer, time } = activity
     return (
         <div className='activity'>
-            <img className='img' src={img} alt="" />
+            <img src={img} alt="" />
             <div className='activity-info'>
                 <div>
-                    <h5>Name :{name}</h5>
+                    <h5>Name : {name}</h5>
                     <p>Trainer : <strong>{trainer}</strong></p>
-                    <p>Time :{time} min</p>
+                    <p>Time : {time} min</p>
                 </div>
                 <button onClick={() => clickHandler(id)}>Add To List</button>
             </div>
